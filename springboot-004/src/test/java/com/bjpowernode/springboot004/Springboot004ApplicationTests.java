e6d46bbffbcff5ec2c9af2d5af65be8a8addd4a4
root <root@123.com>
2023-03-21 22:07:05 +0800

    1

---
+++ springboot-004/src/test/java/com/bjpowernode/springboot004/Springboot004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.springboot004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot004ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
